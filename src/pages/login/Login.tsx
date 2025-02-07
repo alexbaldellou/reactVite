ca72d6b6b46fb41265efaca9b1a24852d6449e95
alexbaldellou@gmail.com <alexbaldellou@gmail.com>
2025-02-07 14:29:04 +0100

    add redirect login to home

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,4 +1,11 @@
+import { useNavigate } from "react-router-dom";
 const Login = () => {
+  const navigate = useNavigate();
+
+  const onSubmit = () => {
+    navigate("/home");
+  };
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-slate-900">
       <form className="flex flex-col bg-slate-800 p-5 rounded-md w-md">
@@ -8,11 +15,16 @@ const Login = () => {
           className="border-2 border-gray-100 rounded-md m-3 p-2 text-white placeholder-slate-300"
         ></input>
         <input
-          type="text"
+          type="password"
           placeholder="Contraseña"
           className="border-2 border-gray-100 rounded-md m-3 p-2 text-white placeholder-slate-300"
         ></input>
-        <button className="m-3">Acceder</button>
+        <button
+          className="m-3 bg-gray-50 text-slate-800 rounded-md p-3"
+          onClick={onSubmit}
+        >
+          Acceder
+        </button>
       </form>
     </div>
   );
